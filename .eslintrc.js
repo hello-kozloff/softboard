c81a434d346b50ae9b04986804634ac273a4a646
hello-kozloff <hello.kozloff@gmail.com>
2022-11-16 22:46:59 +0300

    Create eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'es2021': true
+  },
+  'extends': [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier'
+  ],
+  'parser': '@typescript-eslint/parser',
+  'parserOptions': {
+    'ecmaFeatures': {
+      'jsx': true
+    },
+    'ecmaVersion': 'latest',
+    'sourceType': 'module'
+  },
+  'plugins': [
+    'react',
+    '@typescript-eslint',
+    'prettier'
+  ],
+  'rules': {
+    'react/react-in-jsx-scope': 'off',
+    'camelcase': 'error',
+    'spaced-comment': 'error',
+    'quotes': ['error', 'single'],
+    'no-duplicate-imports': 'error'
+  },
+  'settings': {
+    'import/resolver': {
+      'typescript': {}
+    }
+  }
+}
