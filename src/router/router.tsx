6f7509a830ccf2387119f4d8327b9b6784f532ac
hello-kozloff <hello.kozloff@gmail.com>
2022-11-17 01:39:42 +0300

    Use route validator

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,9 +1,19 @@
+import * as React from 'react'
+import ProtectedRoute from 'components/containers/ProtectedRoute'
 import { createBrowserRouter } from 'react-router-dom'
 import { routes } from './routes'
 
 export const router = createBrowserRouter(
-  routes.map((Route) => ({
-    path: Route.path,
-    element: <Route.component />,
-  })),
+  routes.map((Route) => {
+    const RouteValidator = Route.protected ? ProtectedRoute : React.Fragment
+
+    return {
+      path: Route.path,
+      element: (
+        <RouteValidator>
+          <Route.element />
+        </RouteValidator>
+      ),
+    }
+  }),
 )
