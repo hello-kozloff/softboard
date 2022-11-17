087d9beffaee84beed0a41f4069f9c3306ca864b
hello-kozloff <hello.kozloff@gmail.com>
2022-11-17 22:04:51 +0300

    Auto auth

--- src/pages/Auth/SignIn/SignIn.tsx
+++ src/pages/Auth/SignIn/SignIn.tsx
@@ -1,7 +1,28 @@
 import * as React from 'react'
 import Layout from 'components/core/Layout'
+import { useDispatch, useSelector } from 'react-redux'
+import { UserAction } from 'store/slices/user'
+import { RootState } from 'store'
 
 export const SignIn = () => {
+  const user = useSelector((state: RootState) => state.user)
+  const dispatch = useDispatch()
+
+  React.useEffect(() => {
+    if (!user) {
+      dispatch(
+        UserAction.signIn({
+          id: '',
+          email: '',
+          emailVerifyAt: null,
+          firstName: '',
+          lastName: '',
+          avatar: null,
+        }),
+      )
+    }
+  }, [user])
+
   return (
     <Layout>
       <h1>Welcome to SignIn</h1>
