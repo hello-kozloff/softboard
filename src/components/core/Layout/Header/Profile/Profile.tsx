2c82a82f53d4edfcae50fbf9f13673b2b0f7b879
hello-kozloff <hello.kozloff@gmail.com>
2022-11-18 01:13:07 +0300

    Add chevron down

--- src/components/core/Layout/Header/Profile/Profile.tsx
+++ src/components/core/Layout/Header/Profile/Profile.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import * as Styled from './styled'
 import useUser from 'hooks/useUser'
+import { FiChevronDown } from 'react-icons/fi'
 
 export const Profile = () => {
   const user = useUser()
@@ -9,6 +10,7 @@ export const Profile = () => {
     <Styled.Profile>
       <Styled.Username>{`${user.firstName} ${user.lastName[0]}.`}</Styled.Username>
       <Styled.Avatar src={user.avatar || '/img/not-found-avatar.jpeg'} />
+      <FiChevronDown />
     </Styled.Profile>
   )
 }
