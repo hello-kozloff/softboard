2482120f6bf43f67d940f89bd6ffd3b49c72e8c4
hello-kozloff <hello.kozloff@gmail.com>
2022-11-17 22:04:37 +0300

    Persist store

--- src/components/core/App/App.tsx
+++ src/components/core/App/App.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react'
 import { RouterProvider } from 'react-router-dom'
 import { Provider as StoreProvider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
+import { persistStore } from 'redux-persist'
 import Store from 'store'
 import Styles from 'styles'
 import Router from 'router'
@@ -10,7 +12,9 @@ export const App = () => {
     <React.Fragment>
       <Styles />
       <StoreProvider store={Store}>
-        <RouterProvider router={Router} />
+        <PersistGate loading={null} persistor={persistStore(Store)}>
+          <RouterProvider router={Router} />
+        </PersistGate>
       </StoreProvider>
     </React.Fragment>
   )
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,8 +1,18 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
+import { persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 import UserReducer from './slices/user'
 
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
 export const store = configureStore({
-  reducer: {
-    user: UserReducer,
-  },
+  reducer: persistReducer(
+    persistConfig,
+    combineReducers({
+      user: UserReducer,
+    }),
+  ),
 })
