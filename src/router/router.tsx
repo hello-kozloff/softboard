1ee0347a2158b6cd6163a924421394f689a73939
hello-kozloff <hello.kozloff@gmail.com>
2022-11-17 21:25:50 +0300

    Small changes

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -4,14 +4,14 @@ import { createBrowserRouter } from 'react-router-dom'
 import { routes } from './routes'
 
 export const router = createBrowserRouter(
-  routes.map((Route) => {
-    const RouteValidator = Route.protected ? ProtectedRoute : React.Fragment
-
+  routes.map((route) => {
+    const Page = route.element
+    const RouteValidator = route.protected ? ProtectedRoute : React.Fragment
     return {
-      path: Route.path,
+      path: route.path,
       element: (
         <RouteValidator>
-          <Route.element />
+          <Page />
         </RouteValidator>
       ),
     }
