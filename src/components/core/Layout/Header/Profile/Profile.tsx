960729d29d78a09faa3ffc87ec02a9b415654f41
hello-kozloff <hello.kozloff@gmail.com>
2022-11-18 01:44:30 +0300

    Add profile dropdown

--- src/components/core/Layout/Header/Profile/Profile.tsx
+++ src/components/core/Layout/Header/Profile/Profile.tsx
@@ -1,7 +1,45 @@
 import * as React from 'react'
 import * as Styled from './styled'
 import useUser from 'hooks/useUser'
+import Dropdown, { DropdownProps } from 'components/common/Dropdown'
 import { FiChevronDown } from 'react-icons/fi'
+
+const dropdownConfig: DropdownProps['config'] = [
+  [
+    {
+      to: '/profile',
+      children: <span>Your profile</span>,
+    },
+    {
+      to: '/projects',
+      children: <span>Your projects</span>,
+    },
+    {
+      to: '/boards',
+      children: <span>Your boards</span>,
+    },
+    {
+      to: '/tasks',
+      children: <span>Your tasks</span>,
+    },
+  ],
+  [
+    {
+      to: '/profile/upgrade',
+      children: <span>Upgrade</span>,
+    },
+    {
+      to: '/profile/settings',
+      children: <span>Settings</span>,
+    },
+  ],
+  [
+    {
+      to: '/auth/sign-out',
+      children: <span>Sign out</span>,
+    },
+  ],
+]
 
 export const Profile = () => {
   const user = useUser()
@@ -10,7 +48,9 @@ export const Profile = () => {
     <Styled.Profile>
       <Styled.Username>{`${user.firstName} ${user.lastName[0]}.`}</Styled.Username>
       <Styled.Avatar src={user.avatar || '/img/not-found-avatar.jpeg'} />
-      <FiChevronDown />
+      <Dropdown config={dropdownConfig}>
+        <FiChevronDown />
+      </Dropdown>
     </Styled.Profile>
   )
 }
--- src/components/core/Layout/Header/Profile/styled.ts
+++ src/components/core/Layout/Header/Profile/styled.ts
@@ -1,10 +1,16 @@
 import styled from 'styled-components'
+import { StyledDropdown } from 'components/common/Dropdown'
 
 export const Profile = styled.div`
   display: inline-grid;
   grid-auto-flow: column;
   grid-column-gap: 12px;
   align-items: center;
+
+  ${StyledDropdown} {
+    top: 100%;
+    right: 0;
+  }
 `
 
 export const Username = styled.span`
