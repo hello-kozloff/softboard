af836a36ac2a590dbf821028e3449d31ce6cd8a7
hello-kozloff <hello.kozloff@gmail.com>
2022-11-19 02:26:03 +0300

    Max content

--- src/components/core/Layout/Main/styled.ts
+++ src/components/core/Layout/Main/styled.ts
@@ -1,6 +1,11 @@
 import styled from 'styled-components'
+import { Container } from 'components/common/Container/styled'
 
 export const Main = styled.main`
   box-sizing: border-box;
   padding: 32px 0;
+
+  ${Container} {
+    height: 100%;
+  }
 `
