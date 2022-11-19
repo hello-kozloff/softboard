7508d3943086f7a1706b94d90115bb8ac750ed06
hello-kozloff <hello.kozloff@gmail.com>
2022-11-19 19:20:14 +0300

    Change padding

--- src/pages/Auth/styled.ts
+++ src/pages/Auth/styled.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 
 export const Container = styled.div`
   height: 100%;
-  padding: 82px 16px;
+  padding: 32px 16px;
   box-sizing: border-box;
   text-align: center;
 `
