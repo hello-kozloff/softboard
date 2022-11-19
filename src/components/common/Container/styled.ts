a19eb0ce6953cd4d11c2ebc7e3718297fbbf2e7f
hello-kozloff <hello.kozloff@gmail.com>
2022-11-19 20:18:30 +0300

    Change container width

--- src/components/common/Container/styled.ts
+++ src/components/common/Container/styled.ts
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 export const Container = styled.div`
   width: 100%;
   box-sizing: border-box;
-  max-width: calc(1920px + (16px * 2));
+  max-width: calc(1180px + (16px * 2));
   padding: 0 16px;
   margin: 0 auto;
 `
