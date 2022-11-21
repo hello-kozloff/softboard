370e69eef3f060ee80cfe87b7ee45cda4f6f2eab
hello-kozloff <hello.kozloff@gmail.com>
2022-11-21 22:38:04 +0300

    Change variable

--- src/components/common/Container/styled.ts
+++ src/components/common/Container/styled.ts
@@ -1,10 +1,9 @@
 import styled from 'styled-components'
-import { sizes } from 'styles'
 
 export const Container = styled.div`
   width: 100%;
   box-sizing: border-box;
-  max-width: calc(${sizes.Desktop} + (16px * 2));
+  max-width: calc(1920px + (16px * 2));
   padding: 0 16px;
   margin: 0 auto;
 `
