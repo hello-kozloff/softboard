71814f19e97a71aacab81b09596b91df51094c26
hello-kozloff <hello.kozloff@gmail.com>
2022-11-22 01:07:50 +0300

    Render board

--- src/pages/Boards/View/View.tsx
+++ src/pages/Boards/View/View.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react'
+import Board from 'components/core/Board'
 
 export const View = () => {
-  return <div>Welcome to board</div>
+  return <Board id="84b3322a-fe92-4dcc-84a3-ad0b6499a73f" />
 }
