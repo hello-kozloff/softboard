d3ed6be084f522c9e401b1bf161fced9c7099433
hello-kozloff <hello.kozloff@gmail.com>
2022-11-21 22:38:17 +0300

    Remove unused ContextPayload

--- src/components/core/Layout/types.ts
+++ src/components/core/Layout/types.ts
@@ -1,10 +1,3 @@
-import * as React from 'react'
-
-export interface ContextPayload {
-  headerState: [JSX.Element, React.Dispatch<React.SetStateAction<JSX.Element>>]
-  footerState: [JSX.Element, React.Dispatch<React.SetStateAction<JSX.Element>>]
-}
-
 export interface LayoutProps {
   header?: JSX.Element
   footer?: JSX.Element
