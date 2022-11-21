811dd87b45e0b872dc54e36d28cccc23418a22ca
hello-kozloff <hello.kozloff@gmail.com>
2022-11-22 01:06:37 +0300

    Create supabase client

---
+++ src/supabase/supabase.ts
@@ -0,0 +1,13 @@
+import { createClient } from '@supabase/supabase-js'
+
+const url = process.env.REACT_APP_SUPABASE_URL
+const key = process.env.REACT_APP_SUPABASE_ANON_KEY
+
+if ((url || key) === undefined) {
+  throw new Error('It looks like you forgot to set .env')
+}
+
+export const supabase = createClient(
+  process.env.REACT_APP_SUPABASE_URL as string,
+  process.env.REACT_APP_SUPABASE_ANON_KEY as string,
+)
