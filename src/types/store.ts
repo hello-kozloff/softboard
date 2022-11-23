cc5d5548f2b9c23c1fb244da3e48222117ffa36f
hello-kozloff <hello.kozloff@gmail.com>
2022-11-23 15:05:04 +0300

    Create store types

---
+++ src/types/store.ts
@@ -0,0 +1,63 @@
+import { ID } from './utils'
+
+export interface UserState {
+  id: ID
+  email: string
+  firstName: string
+  lastName: string
+  position: string
+  avatar: string | null
+}
+
+export interface ProjectState {
+  id: ID
+  title: string
+  subtitle: string | null
+  author: UserState['id']
+  created: Date
+  updated: Date
+  participants: UserState['id'][]
+  boards: BoardState['id'][]
+}
+
+export interface BoardState {
+  id: ID
+  name: string
+  author: UserState['id']
+  created: Date
+  updated: Date
+  participants: UserState['id'][]
+  project: ProjectState['id']
+  columns: ColumnState['id'][]
+}
+
+export interface ColumnState {
+  id: ID
+  name: string
+  author: UserState['id']
+  created: Date
+  updated: Date
+  board: BoardState['id']
+  tasks: TaskState['id'][]
+}
+
+export interface TaskState {
+  id: ID
+  name: string
+  content: JSON | null
+  author: UserState['id']
+  created: Date
+  updated: Date
+  participants: UserState['id'][]
+  project: ProjectState['id']
+  column: ColumnState['id']
+  tags: TagState['id'][]
+}
+
+export interface TagState {
+  id: ID
+  text: string
+  color: string
+  background: string
+  project: ProjectState['id']
+}
